e6c632433ace05fd243ee883f750fcf93b3ead37
luiscfernandes <luiscfernandes@deloitte.pt>
2019-04-26 11:36:10 +0100

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongoose').MongoClient;
+//Create a database named "mydb":
+var url = "mongodb://localhost:8089/mydb";
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+  MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      console.log("Database created!");
+      //db.close();
+    });
+    
+
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
