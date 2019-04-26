122df22d449d7c87bba92d49466903a3f91759b6
luiscfernandes <luiscfernandes@deloitte.pt>
2019-04-26 11:39:00 +0100

    Novo comentário

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
   MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       console.log("Database created!");
-      //db.close();
+      //db.close(); //Comentário
     });
     
 
